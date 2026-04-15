e7f4e39e5559e71352eac245b7d9bbc3c3101cee
Semih Özsoy <semih__ozsoy@hotmail.com>
2026-04-15 23:17:23 +0300

    init project

---
+++ bitaksi-case/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bitaksi-case
+//
+//  Created by Semih Özsoy on 15.04.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
